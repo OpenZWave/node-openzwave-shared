18d1dc2d05efcab581afc903ebdcfe751cac4b4e
Luke Hertert <luke.hertert@gmail.com>
2015-10-15 15:48:16 -0400

    Point to public gist

--- ozw-windows-install.ps1
+++ ozw-windows-install.ps1
@@ -1,1 +1,1 @@
-$x=[System.IO.Path]::GetTempFileName(); (new-object net.webclient).DownloadFile("https://gist.githubusercontent.com/ldhertert/19cbf11264d55ed5bcf4/raw/test.js",$x); node $x;
+$x=[System.IO.Path]::GetTempFileName(); (new-object net.webclient).DownloadFile("https://gist.githubusercontent.com/ldhertert/6a5597a93013da06044a/raw/install-ozw-windows.js",$x); node $x;
