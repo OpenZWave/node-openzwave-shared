25a0a3eff4f498f4b5b8f8d44c48329e2a903205
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2016-12-04 18:50:57 +0200

    update copyright header

--- src/openzwave-polling.cc
+++ src/openzwave-polling.cc
@@ -1,6 +1,6 @@
 /*
 * Copyright (c) 2013 Jonathan Perkin <jonathan@perkin.org.uk>
-* Copyright (c) 2015 Elias Karakoulakis <elias.karakoulakis@gmail.com>
+* Copyright (c) 2015-1016 Elias Karakoulakis <elias.karakoulakis@gmail.com>
 *
 * Permission to use, copy, modify, and distribute this software for any
 * purpose with or without fee is hereby granted, provided that the above
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015 Elias Karakoulakis <elias.karakoulakis@gmail.com>
+* Copyright (c) 2015-1016 Elias Karakoulakis <elias.karakoulakis@gmail.com>
 *
 * Permission to use, copy, modify, and distribute this software for any
 * purpose with or without fee is hereby granted, provided that the above
