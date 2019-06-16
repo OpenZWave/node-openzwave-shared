4a93bd5f32f2041a6ba2d91a414889db40f4fff6
Elias Karakoulakis <elias.karakoulakis@gmail.com>
2019-06-16 13:47:25 +0300

    fix merge conflict

--- src/utils.cc
+++ src/utils.cc
@@ -110,15 +110,25 @@ namespace OZW {
 				break;
 			}
 			case OpenZWave::ValueID::ValueType_List: {
-				::std::string val;
-				::std::vector < ::std::string > items;
-				// populate array of all available items in the list
-				OZWManager( GetValueListItems, value, &items);
-				AddArrayOfStringProp(valobj, values, items);
-				// populated selected element
-				OZWManager( GetValueListSelection, value, &val);
-				AddStringProp(valobj, value, val.c_str())
-				break;
+				std::string val;
+ 				int32 val;
+ 				std::vector < std::string > items;
+ 				std::vector < int32> itemsvalues;
+ 				
+ 				// populate array of all available items in the list
+ 				OpenZWave::Manager::Get()->GetValueListItems(value, &items);
+ 				OpenZWave::Manager::Get()->GetValueListValues(value, &itemsvalues);
+ 				
+ 				for (int i = 0; i != items.size(); i++){
+ 					items[i]=std::to_string(itemsvalues[i])+"|"+items[i];
+ 				}
+ 					
+ 				AddArrayOfStringProp(valobj, values, items);
+ 				// populated selected element
+ 				OpenZWave::Manager::Get()->GetValueListSelection(value, &val);
+ 				AddStringProp(valobj, value, val.c_str())
+ 				AddIntegerProp(valobj, value, val);
+ 				break;
 			}
 			case OpenZWave::ValueID::ValueType_Short: {
 				int16 val;
